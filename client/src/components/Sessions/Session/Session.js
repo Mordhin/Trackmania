43bb4a9cf95ac170a2772014b1e8bf61a161ae1d
Julien Corion <astram59@gmail.com>
2021-07-02 18:48:56 +0200

    sessions displayed

--- client/src/components/Sessions/Session/Session.js
+++ client/src/components/Sessions/Session/Session.js
@@ -1,5 +1,17 @@
 import React from "react";
 
-export const Session = () => {
-  return <div>Session</div>;
+export const Session = ({ data }) => {
+  const editSession = () => {};
+
+  const deleteSession = () => {};
+
+  return (
+    <div>
+      <h3>{data.date}</h3>
+      <p>{data.duration} heures</p>
+      <div>{data.creator}</div>
+      <button onClick={editSession}>Edit</button>
+      <button onClick={deleteSession}>Delete</button>
+    </div>
+  );
 };
